3de8f5ee015cd96ae55807a659a4ba2295e3851c
safwen-code <safwendjebbi1234@gmail.com>
2021-09-04 02:57:21 +0100

    Add more code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,41 @@ import "./App.css";
 import Navbars from "./components/Layout/Navbars";
 import { Container } from "react-bootstrap";
 import Users from "./components/Content/users/Users";
+import { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <Container fluid>
-      <Navbars titel = {"github"} />
-      <Users />
-    </Container>
-  );
+import { CLIENT_ID, SECRET_CLIENT } from "./components/Variabal";
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    try {
+      this.setState({ loading: true });
+      const res = await axios.get(`https://api.github.com/users?client_id =${CLIENT_ID}&client_secret=${SECRET_CLIENT}`);
+      console.log("result", res);
+      this.setState({
+        users: res.data,
+      });
+      this.setState({ loading: false });
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+  searchuser = (text) =>{
+    console.log("search user function" , text)
+  }
+  render() {
+    const { users, loading } = this.state;
+    return (
+      <Container fluid>
+        <Navbars titel={"github"} searchuser={this.searchuser} />
+        <Users users={users} loading={loading} />
+      </Container>
+    );
+  }
 }
 
 export default App;
